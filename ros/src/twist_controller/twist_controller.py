02cd2f02a090d502e9323a632418205d943911de
Alexei Strots <strotz@gmail.com>
2017-10-19 20:25:38 -0700

    add simple lpf for acceleration

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -1,6 +1,7 @@
 import math
 from pid import PID
 from yaw_controller import YawController
+from lowpass import LowPassFilter
 
 GAS_DENSITY = 2.858
 ONE_MPH = 0.44704
@@ -12,15 +13,17 @@ class Controller(object):
         self.fuel_capacity = args[1]
         self.brake_deadband = args[2]
         self.decel_limit = args[3]
-        self.accel_limit = args[4]
+        self.acceleration_limit = args[4]
         self.wheel_radius = args[5]
         self.wheel_base = args[6]
         self.steer_ratio = args[7]
-        self.max_lat_accel = args[8]
+        self.max_lat_acceleration = args[8]
         self.max_steer_angle = args[9]
 
-        self.pid_throttle = PID(6.0, 0.25, 0.4, self.decel_limit, self.accel_limit)
-        self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, 0.0, self.max_lat_accel, self.max_steer_angle)
+        self.pid_throttle = PID(6.0, 0.25, 1.0, self.decel_limit, self.acceleration_limit)
+        self.yaw_controller = YawController(self.wheel_base, self.steer_ratio, 0.0, self.max_lat_acceleration, self.max_steer_angle)
+
+        self.acceleration_lpf = LowPassFilter(3, 1)
 
     def control(self, *args, **kwargs):
         proposed_linear_velocity = args[0]
@@ -29,16 +32,17 @@ class Controller(object):
         dt = 0.02
 
         velocity_error = proposed_linear_velocity - current_linear_velocity
-        accel = self.pid_throttle.step(velocity_error, dt)
+        acceleration = self.pid_throttle.step(velocity_error, dt)
+        acceleration = self.acceleration_lpf.filt(acceleration)
 
         steer = self.yaw_controller.get_steering(proposed_linear_velocity, proposed_angular_velocity, current_linear_velocity)
 
-        if accel > 0.0:
-            throttle = accel
+        if acceleration > 0.0:
+            throttle = acceleration
             brake = 0.0
         else:
             throttle = 0.0
-            decel = -accel
+            decel = -acceleration
             if decel < self.brake_deadband:
                 decel = 0.0
 
